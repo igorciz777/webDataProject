45c9168a317fe0deb1115f7aa305c9c9ac77179c
igorciz777 <igorciz777@gmail.com>
2020-12-01 15:46:39 +0100

    First attempt at data extraction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-
+        Document doc = null;
+        try{
+            doc = Jsoup.connect("https://www.autoevolution.com/cars/").get(); //encyklopedia aut
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Element element1 = doc.getAllElements().first();
+        System.out.println(element1.toString());
     }
 }
